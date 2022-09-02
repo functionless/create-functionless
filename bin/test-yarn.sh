1b1c616170c1f606dab58b5cb97e2c2bbb6bbd5d
Tyler van Hensbergen <tyler@functionless.org>
2022-09-02 11:58:09 -0600

    fix: github actions don't support temp directory

--- bin/test-yarn.sh
+++ bin/test-yarn.sh
@@ -4,17 +4,12 @@ set -ue pipefail
 
 PACKAGE_NAME="create-functionless"
 PACKED_NAME="${PACKAGE_NAME}.tgz"
-WORK_DIR=`mktemp -d`
-
-if [[ ! "$WORK_DIR" || ! -d "$WORK_DIR" ]]; then
-  echo "Could not create temp dir"
-  exit 1
-fi
+TEST_PROJECT="test-project"
 
 function cleanup {
   yarn global remove create-functionless || true
-  rm -rf "$WORK_DIR"
-  echo "Deleted temp working directory $WORK_DIR"
+  rm -fr ${PACKED_NAME} || true
+  rm -fr ${TEST_PROJECT} || true
 }
 
 trap cleanup EXIT
@@ -25,13 +20,12 @@ yarn cache clean
 yarn run release
 yarn pack -f ${PACKED_NAME}
 yarn global add --skip-integrity-check --offline "file:$(pwd)/${PACKED_NAME}"
-rm ${PACKED_NAME}
-
-cd $WORK_DIR
 
 # Use the create script to create a new project
-yarn create --offline functionless test-project
-cd test-project
+yarn create --offline functionless ${TEST_PROJECT}
+cd ${TEST_PROJECT}
 
 # Verify new project can synth
 yarn synth
+
+cd ..
